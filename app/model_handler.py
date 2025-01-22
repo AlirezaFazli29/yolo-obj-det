aa68662c78cabe8377aaaf3748b32ba32695064d
AlirezaFazli29 <alirezafazli29@gmail.com>
2025-01-22 13:57:55 +0330

    working with model to get json format output

---
+++ app/model_handler.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class YoloType():
+    """Enumeration for model types."""
+
+    class Pretrained(Enum):
+        yolo8n = "yolo8n.pt"
+        yolo8s = "yolo8s.pt"
+        yolo8m = "yolo8m.pt"
+        yolo8l = "yolo8l.pt"
+        yolo8x = "yolo8x.pt"
+        yolo9n = "yolo9n.pt"
+        yolo9s = "yolo9s.pt"
+        yolo9m = "yolo9m.pt"
+        yolo9l = "yolo9l.pt"
+        yolo9x = "yolo9x.pt"
+        yolo10n = "yolo10n.pt"
+        yolo10s = "yolo10s.pt"
+        yolo10m = "yolo10m.pt"
+        yolo10l = "yolo10l.pt"
+        yolo10x = "yolo10x.pt"
+        yolo11n = "yolo11n.pt"
+        yolo11s = "yolo11s.pt"
+        yolo11m = "yolo11m.pt"
+        yolo11l = "yolo11l.pt"
+        yolo11x = "yolo11x.pt"
+
+    class Custom(Enum):
+        Firearm_last = "models/last(Firearm).pt"
+        Firearm_best = "models/best(Firearm).pt" 
---
+++ app/utils.py
@@ -0,0 +1,41 @@
+from ultralytics.engine.results import Results
+
+
+def process_yolo_result(result: Results) -> dict:
+    """
+    Process the YOLO inference result and group detections by object type.
+
+    This function takes the result from a YOLO inference, which includes detected 
+    objects with their respective bounding boxes and confidence scores, and then 
+    groups these detections by object name (e.g., "person", "car", etc.). It returns 
+    a dictionary containing the object name, the count of detections for each object, 
+    and the associated bounding boxes and confidence scores.
+
+    Args:
+        result (Results): The result from the YOLO model inference, typically containing 
+                           a list of detected objects with their properties (name, 
+                           confidence, bounding box coordinates).
+
+    Returns:
+        dict: A dictionary where the keys are object names (e.g., "person"), and the 
+              values are dictionaries containing:
+              - 'obj': The object name.
+              - 'count': The number of detections for this object.
+              - 'boxes': A list of bounding box data for each detection, including 
+                confidence score and coordinates (x1, y1, x2, y2).
+    """
+    summary = result.summary()
+    if len(summary)>0:
+        grouped = {}
+        for item in summary:
+            obj_name = item['name']
+            if obj_name not in grouped:
+                grouped[obj_name] = {'obj': obj_name, 'count': 0, 'boxes': []}
+            grouped[obj_name]['boxes'].append({'conf': item['confidence'], **item['box']})
+            grouped[obj_name]['count'] += 1
+        return grouped
+    else: 
+        return {
+            "error": 404,
+            "message": "No object detected"
+        }
